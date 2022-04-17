5ada49372560ed0b1f33b57473332f780ed2a9bb
Yog <yog.panjarale@gmail.com>
2022-04-17 12:49:16 +0530

    memes

--- src/commands/slashes.ts
+++ src/commands/slashes.ts
@@ -1,19 +1,19 @@
 import { RateLimit, TIME_UNIT } from "@discordx/utilities";
 import { CommandInteraction, MessageActionRow, MessageButton, MessageEmbed } from "discord.js";
 import { Discord, Guard, Slash, SlashChoice, SlashOption } from "discordx";
-import { airdrop, getBalance } from "../services/faucet.js";
+import { LOWBAL, memeImages, RICHBAL } from "../constants.js";
+import { airdrop, getBalance, PAYER } from "../services/faucet.js";
 import {
 	BalanceResponse,
 	TheBlockChainApi,
 } from "../services/theblockchainapi.js";
 
-// rich balance in lamport
-const RICHBAL = 1;
+
 // no funds image
 const noFunds = new MessageEmbed({
-	title: "No funds available on Mainnet",
+	title: "Low on funds :(",
 	image: {
-		url: "https://cdn.discordapp.com/attachments/958293505065758760/964885492803965018/bernie-sanders-asking-for-financial-support-memes.jpg",
+		url: memeImages[Math.floor(Math.random() * memeImages.length)],
 	},
 });
 const formatAddress = (address: string) => {
@@ -26,7 +26,14 @@ const formatAddress = (address: string) => {
 	}
 };
 const toSol = (amount: number) => amount * 1e-9;
-
+const checkValidAddress = (address: string) => {
+	// check length is 44
+	const length44 = address.length === 44;
+	// check if base58
+	const base58 = /^[A-Za-z0-9]+$/;
+	const isBase58 = base58.test(address);
+	return length44 && isBase58;
+};
 const BApi = new TheBlockChainApi(
 	process.env.API_KEY_ID || "",
 	process.env.API_KEY_SECRET || ""
@@ -53,19 +60,59 @@ export abstract class SlashExample {
 	async ping(interaction: CommandInteraction): Promise<void> {
 		interaction.reply("pong!");
 	}
+	@Slash("mainnet")
+	async mainnet(interaction: CommandInteraction): Promise<void> {
+		// reply with noFunds embed
+		await interaction.deferReply();
+		const embed = new MessageEmbed({
+			title:"Low on funds :(",
+			image: {
+				url: memeImages[Math.floor(Math.random() * memeImages.length)],
+			},
+		});
+		await interaction.editReply({ embeds: [embed] });
+	}
 	@Slash("balance")
 	async balance(
-		@SlashOption("address") address: string,
+		@SlashOption("address",{required:false}) address: string,
+		
 		interaction: CommandInteraction
 	): Promise<void> {
-		await interaction.deferReply();
-		const balance = await BApi.getBalance(address);
-		const error = balance as any;
-		if (error.errors) {
-			showError(error.errors[0].msg, interaction);
-		} else {
-			const bal = balance as BalanceResponse;
+		if (!address) {
+			address = PAYER;
 		}
+		await interaction.deferReply();
+		if (!checkValidAddress(address)) {
+			await showError("Invalid address", interaction);
+			return;
+		}
+
+		const balances:Record<string,number> = {
+			"devnet": 0,
+			"testnet": 0,
+			"mainnet-beta": 0,
+		}
+		// loop over balances
+		for (const network in balances) {
+			try {
+				const balance = await getBalance(address, network);
+				balances[network] = balance.balance;
+			} catch (e) {
+				console.log(e);
+			}
+		}
+		const embed = new MessageEmbed({
+			title: "Balance",
+			description: `${formatAddress(address)}`,
+		});
+		for (const network in balances) {
+			embed.addField(
+				`● ${network}`,
+				`${balances[network]} SOL`,
+			);
+		}
+		await interaction.editReply({ embeds: [embed] });
+		
 	}
 	@Slash("faucet")
 	async faucet(
@@ -75,6 +122,10 @@ export abstract class SlashExample {
 		network: "devnet" | "testnet" | "mainnet-beta",
 		interaction: CommandInteraction
 	): Promise<void> {
+		if (!checkValidAddress(address)) {
+			await showError("Invalid address", interaction);
+			return;
+		}
 		if (!network) network = "devnet"
 		await interaction.deferReply();
 		// if network is mainnet then send mainnet no fund image
@@ -89,8 +140,26 @@ export abstract class SlashExample {
 			const bal = balance as BalanceResponse;
 			// check balance is greater than rich balance
 			if (bal.balance > RICHBAL) {
-				const richEmbed = RichEmbed(bal);
+				const richEmbed = new MessageEmbed({
+					title: "You have more than enough funds, better consider donating some here",
+					url:`https://solscan.io/address/${PAYER}`,
+					thumbnail:{
+						url:"https://cdn.discordapp.com/emojis/865518028979437599.webp?size=128&quality=lossless"
+					}
+				});
+
 				await interaction.editReply({ embeds: [richEmbed] });
+				return;
+			}
+		} catch (error:any) {
+			await showError(error.message, interaction);
+			return;
+		}
+		// check payer balance
+		try {
+			const payerBal = await getBalance(PAYER, network);
+			if (payerBal.balance < LOWBAL[network]) {
+				await interaction.editReply({ embeds: [noFunds] });
 				return;
 			}
 		} catch (error:any) {
@@ -104,9 +173,11 @@ export abstract class SlashExample {
 				const val = (await airdropResponse.next()).value;
 				if (val.state=="processing"){
 					const embed = new MessageEmbed({
-						title: "Airdrop",
-						description: `Airdrop is processing. Please wait.`,
+						title: "Airdrop is processing. Please wait.",
 						color: "#DE1738",
+						thumbnail:{
+							url: "https://c.tenor.com/hRBZHp-kE0MAAAAC/loading-circle-loading.gif",
+						}
 					});
 					await interaction.editReply({ embeds: [embed] });
 				}
@@ -120,7 +191,7 @@ export abstract class SlashExample {
 						color: "#DE1738",
 					});
 					const linkButton = new MessageButton();
-					linkButton.setLabel("View on Solana Explorer").setStyle("LINK").setURL(val.message)
+					linkButton.setLabel("View on solscan").setStyle("LINK").setURL(val.message)
 					const actionrow = new MessageActionRow();
 					actionrow.addComponents(linkButton);
 					await interaction.editReply({ embeds: [embed] ,components: [actionrow] });
@@ -133,12 +204,5 @@ export abstract class SlashExample {
 			return 
 		} 
 	}
-}
-function RichEmbed(bal: BalanceResponse) {
-	return new MessageEmbed({
-		title: "You have enough funds",
-		description: `You have ${bal.balance} SOL`,
-		color: "#DE1738",
-	});
 }
 
---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const RICHBAL = 1;
+export const LOWBAL= {
+    "devnet": 0.1,
+    "testnet":0.5,
+    "mainnet-beta":0.001,
+} ;
+export const memeImages = [
+    "https://cdn.discordapp.com/attachments/958293505065758760/964885492803965018/bernie-sanders-asking-for-financial-support-memes.jpg",
+
+]
